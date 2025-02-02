c3cfb2566135d3be6993c9991eb4b1ae794f7cdd
Benjamin Patch <ben@benjaminpatch.com>
2025-02-01 17:29:36 -0800

    Round estimated time.

--- travel.py
+++ travel.py
@@ -1,4 +1,12 @@
+# print welcome message
 def trip_planner_welcome(name):
     print("Welcome to Trip Planner v1.0", name)
 
 trip_planner_welcome("Ben")
+
+# estimated time
+def estimated_time_rounded(estimated_time):
+    rounded_time = round(estimated_time)
+    return rounded_time
+
+estimate = estimated_time_rounded(3.5)
