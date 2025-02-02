2d32c6057e86fcf41efdfa129cb2db656d0f98ba
Benjamin Patch <ben@benjaminpatch.com>
2025-02-02 12:29:26 -0800

    Write a function that will generate messages for the planned trip.

--- travel.py
+++ travel.py
@@ -13,4 +13,7 @@ estimate = estimated_time_rounded(3.5)
 
 # generate messages for the planned trip
 def destination_setup(origin, destination, estimated_time, mode_of_transport="Car"):
-    pass
+    print("Your trip starts off in", origin)
+    print("And you are traveling to", destination)
+    print("You will be traveling by", mode_of_transport)
+    print("It will take approximately " + str(estimated_time) + " hours")
